aaeae112a770c241efb7f1dcb43dd60cda5170e0
Preethi-josline <preethijosline1@gmail.com>
2026-05-20 00:23:39 +0530

    fixed backend api url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://crimsonnet-backend.onrender.com',
         changeOrigin: true,
       },
     },
